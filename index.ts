9b95886e90e031b723898304019cd56a49098a47
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2023-01-31 13:53:15 +0900

    add sort feature

--- index.ts
+++ index.ts
@@ -27,9 +27,15 @@ const generate_palette = (base_color: string, palette_size_hue: number, palette_
         color.hue = hue;
         color.chroma = base_chroma;
         color.tone = base_tone;
-        return hexFromArgb(color.toInt());
+        return {
+            hex: hexFromArgb(color.toInt()),
+            hue: hue
+        };
     });
-    palette.push(row);
+    palette.push({
+        tone: base_tone,
+        colors: row
+    });
     for (let i = 0; i < palette_size_tone - 1; i++) {
         if (color.tone + tone_step <= 100) {
             color.tone += tone_step;
@@ -44,23 +50,31 @@ const generate_palette = (base_color: string, palette_size_hue: number, palette_
             color.hue = hue;
             color.chroma = base_chroma;
             color.tone = tone;
-            return hexFromArgb(color.toInt());
+
+            return {
+                hex: hexFromArgb(color.toInt()),
+                hue: hue
+            };
         });
-        palette.push(row);
+        palette.push({
+            tone: tone,
+            colors: row
+        });
     }
 
     // Error correction of base_color
-    palette[0][0] = base_color.toLowerCase();
+    palette[0].colors[0].hex = base_color.toLowerCase();
 
     return palette;
 };
 
-const generate_button = document.getElementById("generate_button")!;
+const generate_button = document.querySelector("#generate_button")!;
+const sort_button = document.querySelector("#sort_button")!;
 const base_color_element: HTMLInputElement = document.querySelector("#base_color")!;
 const palette_size_hue_element: HTMLInputElement = document.querySelector("#palette_size_hue")!;
 const palette_size_tone_element: HTMLInputElement = document.querySelector("#palette_size_tone")!;
-const palette_element = document.getElementById("palette")!;
-const download_button = document.getElementById("download_button")!;
+const palette_element: HTMLElement = document.querySelector("#palette")!;
+const download_button = document.querySelector("#download_button")!;
 
 generate_button.addEventListener("click", () => {
     const base_color = base_color_element.value;
@@ -75,18 +89,41 @@ generate_button.addEventListener("click", () => {
 
     for (const row of palette) {
         const row_outer = document.createElement("div");
+        row_outer.dataset.tone = row.tone.toString();
 
-        for (const color of row) {
+        for (const color of row.colors) {
             const div = document.createElement("div");
-            div.style.background = color;
-            div.title = color;
+            div.style.background = color.hex;
+            div.title = color.hex;
+            div.dataset.hue = color.hue.toString();
             div.addEventListener("click", () => {
-                navigator.clipboard.writeText(color);
+                navigator.clipboard.writeText(color.hex);
             });
             row_outer.appendChild(div);
         }
 
         palette_element.appendChild(row_outer);
+    }
+
+    palette_element.querySelector("[data-hue]")!.classList.add("base_color");
+});
+
+sort_button.addEventListener("click", () => {
+    const row_list = [...palette_element.children] as Array<HTMLElement>;
+    row_list.sort((a, b) => {
+        return parseFloat(b.dataset.tone || "0") - parseFloat(a.dataset.tone || "0");
+    });
+
+    for (const row of row_list) {
+        const children = [...row.children] as Array<HTMLElement>;
+        children.sort((a, b) => {
+            return parseFloat(a.dataset.hue || "0") - parseFloat(b.dataset.hue || "0");
+        });
+        for (const child of children) {
+            row.appendChild(child);
+        }
+
+        palette_element.appendChild(row);
     }
 });
 
