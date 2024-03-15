48f151bca8d098efb48301adbe594f53be75d44f
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2024-03-16 00:12:14 +0900

    refactor: create UI class (#25)

--- figma/ui_script.ts
+++ figma/ui_script.ts
@@ -1,23 +1,6 @@
-import "../common/side_effect";
-import "../common/style.css";
-import { Message, MessageGenerate } from "../types/figma";
-import { Palette } from "../common/palette";
-import { Renderer } from "../common/renderer";
-
-const baseColorInput: HTMLInputElement = document.querySelector("#base_color")!;
-const baseColorPicker: HTMLInputElement = document.querySelector("#base_color_picker")!;
-const baseColorPreview: HTMLInputElement = document.querySelector("#base_color_preview")!;
-const generateButton = document.getElementById("generate_button")!;
-const hueSizeInput: HTMLInputElement = document.querySelector("#palette_size_hue")!;
-const toneSizeInput: HTMLInputElement = document.querySelector("#palette_size_tone")!;
-const paletteOuter = document.getElementById("palette")!;
-const sortButton = document.getElementById("sort_button")!;
-const insertButton = document.getElementById("action_button")!;
-
-const palette = new Palette();
-const renderer = new Renderer(paletteOuter);
-
-insertButton.textContent = "Insert to Figma";
+import { Message } from "../types/figma";
+import { PaletteData } from "../common/palette";
+import { UI } from "../common/ui";
 
 /**
  * Send message to the backend script.
@@ -27,63 +10,26 @@ const postMessage = (message: Message): void => {
     parent.postMessage({ pluginMessage: message }, "*");
 };
 
-/**
- * Check all form values are valid.
- * @returns Validation result.
- */
-const validateForm = (): boolean =>
-    baseColorInput.checkValidity() && hueSizeInput.checkValidity() && toneSizeInput.checkValidity();
-
-baseColorInput.addEventListener("input", () => {
-    baseColorPreview.style.background = baseColorInput.value;
-});
-
-baseColorPreview.addEventListener("click", () => {
-    baseColorPicker.click();
-});
-
-baseColorPicker.addEventListener("input", () => {
-    baseColorInput.value = baseColorPicker.value;
-    baseColorPreview.style.background = baseColorPicker.value;
-});
-
-generateButton.addEventListener("click", () => {
-    if (!validateForm()) {
-        const message: Message = {
-            message: "Parameters are invalid. Please check the form.",
-            option: {
-                error: true,
-                timeout: 5000
-            },
-            type: "notify"
-        } as const;
-        postMessage(message);
-        return;
-    }
-
-    const baseColor = baseColorInput.value;
-    const message = {
-        baseColor,
-        hueSize: parseInt(hueSizeInput.value, 10),
-        toneSize: parseInt(toneSizeInput.value, 10),
-        type: "generate"
-    } as const;
-    postMessage(message);
-});
-
-window.addEventListener("message", (event: MessageEvent<{ pluginMessage: MessageGenerate }>) => {
-    const { baseColor, hueSize, toneSize } = event.data.pluginMessage;
-    const paletteData = palette.generate(baseColor, hueSize, toneSize);
-    renderer.renderPalette(paletteData);
-});
-
-sortButton.addEventListener("click", () => {
-    const paletteData = palette.sortPalette();
-    renderer.renderPalette(paletteData);
-});
-
-insertButton.addEventListener("click", () => {
-    const paletteData = palette.getPalette();
-    if (!paletteData) throw new Error("Palette data is not generated.");
+const onActionButtonClick = (paletteData: PaletteData): void => {
     postMessage({ palette: paletteData, type: "insert" });
-});
+};
+
+const onErrorMessage = (message: string): void => {
+    const messageData: Message = {
+        message,
+        option: {
+            error: true,
+            timeout: 5000
+        },
+        type: "notify"
+    } as const;
+    postMessage(messageData);
+};
+
+const UIOptions = {
+    actionButtonLabel: "Insert to Figma",
+    onActionButtonClick,
+    onErrorMessage
+};
+
+new UI(UIOptions);
